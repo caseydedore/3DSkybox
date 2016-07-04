9980d1787194164c166ec2b3ea407b3dfc1bfc34
caseydedore <caseydedore@gmail.com>
2016-07-03 19:25:54 -0700

    Initial Commit

---
+++ Assets/CopyPosition.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+
+public class CopyPosition : MonoBehaviour
+{
+    [SerializeField]
+    private float copyFactor = 1f;
+    [SerializeField]
+    private Transform target;
+
+    private Vector3 targetLastPosition = Vector3.zero;
+    private float targetTravelDistance = 0f;
+
+
+    void Start()
+    {
+        targetLastPosition = target.position;
+    }
+
+    void Update()
+    {
+        targetTravelDistance = Vector3.Distance(target.position, targetLastPosition);
+
+        transform.position = transform.position + ((target.position - targetLastPosition) * copyFactor);
+
+        targetLastPosition = target.position;
+    }
+}
---
+++ Assets/CopyRotation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CopyRotation : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+	void Update ()
+    {
+        transform.rotation = target.rotation;
+	}
+}
