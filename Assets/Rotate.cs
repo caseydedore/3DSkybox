72e7f2eb7663199d3f4f3e66e8ef45b51b76277b
caseydedore <caseydedore@gmail.com>
2016-07-13 22:39:21 -0700

    Added cloudbox scene.

---
+++ Assets/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 rotationAxes;
+
+    void LateUpdate()
+    {
+        transform.rotation *= Quaternion.Euler(rotationAxes * Time.deltaTime);
+    }
+}
